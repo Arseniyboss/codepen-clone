128f52d74a250ee7d254229b1a53e59553e2ac7d
Arseniy Zakharchenko <arseniyboss5@gmail.com>
2024-04-21 09:38:10 +0200

    replace vh with dvh for mobile devices

--- src/styles.tsx
+++ src/styles.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const EditorSection = styled.section`
-  height: 50vh;
+  height: 50dvh;
   display: grid;
   grid-template-columns: repeat(3, minmax(340px, 1fr));
   gap: var(--spacing);
